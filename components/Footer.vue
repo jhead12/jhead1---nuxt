90304ceba6e6698a1186804f176500fe6686cae1
Josh Head <jhead12@users.noreply.github.com>
2025-01-21 01:39:04 -0800

    update

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="my-20">
     <p class="text-sm text-center text-zinc-600 ">
-      Copyright &copy; {{ new Date().getFullYear() }} Josh Powered by <a href="https://www.joshuahead.com/" class="underline">Josh!</a>.
+      Copyright &copy; {{ new Date().getFullYear() }} Josh Powered by <a href="https://www.linkedin.com/in/joshuahead/" class="underline">Josh!</a>.
     </p>
   </footer>
 </template>

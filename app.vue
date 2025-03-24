ceeefc6d8c3a7501eeecba70dcb0c044b83eda09
Josh Head <jhead12@users.noreply.github.com>
2025-03-24 05:28:11 -0700

    update

--- app.vue
+++ app.vue
@@ -3,8 +3,8 @@ useHead({
   htmlAttrs: { lang: 'en-US' },
   title: 'Joshua Head Portfolio Site',
   meta: [
-    { name: 'description', content: 'Joshua Head is the best in tbe business' }
-    { name:"google-site-verification", content:"gzkS58btf52DI-lAEdYLHHpzJC8c1Q04kx7lLnYIkMs"}
+    { name: 'description', content: 'Joshua Head is the best in tbe business' },
+    { name:"google-site-verification", content: "gzkS58btf52DI-lAEdYLHHpzJC8c1Q04kx7lLnYIkMs"}
   ],
 })
 </script>
